ee26388ef3d0c1279e73054ad68f075f8edbcdb7
Mohammad Barzegar <barzegar102@gmail.com>
2024-02-04 13:10:37 -0800

    Added index.html and new function in test.py to count
    Sample descripton for the description box 123

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 print("Hy!")
+
+
+def count_to_ten(fromz):
+    from_orig = fromz
+    for i in range(from_orig):
+        fromz += 1
+        print(f"{fromz - 1} added with 1 is {fromz}")
+    return
+
+
+if __name__ == '__main__':
+    count_to_ten(10)
